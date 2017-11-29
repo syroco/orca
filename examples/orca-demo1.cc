2a04d8df52900ee16059bc4556b3db7be188060d
vpadois <vincent.padois@gmail.com>
2017-11-29 10:56:03 +0100

    Adding some helpful comments on the internals

--- examples/orca-demo1.cc
+++ examples/orca-demo1.cc
@@ -1,4 +1,4 @@
-#include <orca/orca.h>
+#include <orca/orca.h> //this implicitly creates an optimization problem
 using namespace orca;
 using namespace orca::common;
 using namespace orca::optim;
@@ -10,12 +10,12 @@ using namespace orca::util;
 
 int main(int argc, char const *argv[])
 {
-    
+
     orca::util::Logger::parseArgv( argc, argv );
 
     if(argc < 2)
     {
-        std::cerr << "Usage : ./orca-demo1 /path/to/robot-urdf.urdf" << "\n";
+        std::cerr << "Usage : ./orca-demosimple /path/to/robot-urdf.urdf" << "\n";
         return -1;
     }
 
@@ -34,16 +34,18 @@ int main(int argc, char const *argv[])
     robot->setGravity(Eigen::Vector3d(0,0,-9.81));
 
     EigenRobotState state;
-    state.setFixedBase();
+    state.setFixedBase(); // sets world to base to identity and base velocity to zero
     state.resize(robot->getNrOfDegreesOfFreedom());
 
+    state.jointPos.setZero();
+    state.jointVel.setZero();
     robot->setRobotState(state.jointPos,state.jointVel);
 
-    std::cout << "===== Cartesian Task" << '\n';
+    std::cout << "===== Cartesian Task creation" << '\n';
 
     CartesianTask cart_task;
     cart_task.setName("CartTask-link_7");
-    cart_task.setRobotModel(robot);
+    cart_task.setRobotModel(robot); // Create and Resize the models associated to the task
     cart_task.setControlFrame("link_7");
 
     Eigen::Affine3d cart_pos_ref;
@@ -84,7 +86,7 @@ int main(int argc, char const *argv[])
 
     cart_task.setDesired(cart_acc_pid.getCommand());
     std::cout << "===== Cartesian update()" << '\n';
-    cart_task.update();
+    cart_task.update(); //The state of the task is now "initialized" (computes for the first time the associated cost). It can now be used.
 
     std::cout << "===== Contacts" << '\n';
 
@@ -120,7 +122,7 @@ int main(int argc, char const *argv[])
     dynConstr.setRobotModel(robot);
     dynConstr.update();
 
-    std::cout << "===== Regularisation" << '\n';
+    std::cout << "===== Joint limits" << '\n';
 
     const int ndof = robot->getNrOfDegreesOfFreedom();
 
@@ -129,13 +131,15 @@ int main(int argc, char const *argv[])
     Eigen::VectorXd jntTrqMax;
     jntTrqMax.resize(ndof);
     jntTrqMax.setConstant(200.0);
+    jntTrqMax<<200,200,200,200,120,120,120;
     jnt_trq_cstr.setRobotModel(robot);
-    jnt_trq_cstr.setLimits(-jntTrqMax,jntTrqMax);
+    jnt_trq_cstr.setLimits(-jntTrqMax,jntTrqMax); // because not red in the URDF
     jnt_trq_cstr.update();
 
     JointPositionLimitConstraint jnt_pos_cstr;
     jnt_pos_cstr.setName("JointPositionLimit");
-    jnt_pos_cstr.setRobotModel(robot);
+    jnt_pos_cstr.setRobotModel(robot);  // red in the URDF so no need to set
+    jnt_pos_cstr.setJointLimitsFromRobotModel(); //optional, see previous comment
     jnt_pos_cstr.update();
 
     JointVelocityLimitConstraint jnt_vel_cstr;
@@ -147,6 +151,8 @@ int main(int argc, char const *argv[])
     jnt_vel_cstr.setLimits(-jntVelMax,jntVelMax);
     jnt_vel_cstr.update();
 
+    // There is no such thing as an acceleration constraint...
+
     JointAccelerationLimitConstraint jnt_acc_cstr;
     Eigen::VectorXd jntAccMax;
     jntAccMax.resize(ndof);
@@ -156,9 +162,12 @@ int main(int argc, char const *argv[])
     jnt_acc_cstr.setLimits(-jntAccMax,jntAccMax);
     jnt_acc_cstr.update();
 
+    std::cout << "==== Regularisation" << '\n';
 
-    AccelerationRegularisationTask acc_reg_task;
-    RegularisationTask<ControlVariable::X> reg_task;
+    RegularisationTask<ControlVariable::X> reg_task; // whole vector
+
+    AccelerationRegularisationTask acc_reg_task; // Only on joint acc
+    //RegularisationTask<ControlVariable::GeneralisedJointAcceleration> acc_reg_SAME_task; // equivalent to above task
     TorqueRegularisationTask trq_reg_task;
     WrenchRegularisationTask wrench_reg_task1;
     WrenchRegularisationTask wrench_reg_task2;
@@ -190,16 +199,21 @@ int main(int argc, char const *argv[])
     wrench_reg_task4.EuclidianNorm().setWeight(1E-3);
 
     reg_task.update();
-    reg_task.insertInProblem();
     acc_reg_task.update();
     trq_reg_task.update();
     wrench_reg_task1.update();
     wrench_reg_task2.update();
     wrench_reg_task3.update();
     wrench_reg_task4.update();
-    
+
     LOG_INFO << "===== Inserting Tasks" << '\n';
     // Insert Tasks/Constraints in Problem
+    // Insertion is useful to avoid dynamic allocations due to potentially appearing and disappearing tasks and Constraints
+    // Insertion may resize the optim variable and the QP so insertion is time consumming and should be performed only rarely
+    // Ideally only at the problem creation time. Inserted tasks can also be removed. This may also imply some resizing.
+    // The task and constraints are actually integrated in the QP when their internal state is switched to "activated"
+    // Activation does not induce any resizing so is to be preferred while in the loop
+    reg_task.insertInProblem(); // This so called "problem" is the one implicitly created when including the header ocra.h
     cart_task.insertInProblem();
     reg_task.insertInProblem();
 
@@ -212,7 +226,9 @@ int main(int argc, char const *argv[])
     contact2.insertInProblem();
     contact3.insertInProblem();
     contact4.insertInProblem();
-    
+
+    std::cout<<"===== Activating tasks and constraints"<<'\n';
+
     cart_task.activate();
     reg_task.activate();
 
@@ -222,23 +238,24 @@ int main(int argc, char const *argv[])
     jnt_vel_cstr.activate();
     jnt_acc_cstr.activate();
 
+    // Deactivated by default, just to make clear those four are
     contact1.desactivate();
     contact2.desactivate();
     contact3.desactivate();
     contact4.desactivate();
-    
+
     std::cout << "===== Qp Solver" << '\n';
 
     WeightedQPSolver qp;
-    qp.resize();
+    qp.resize(); //allocation based on the created optimization problem
     qp.setPrintLevel(0);
 
     timer.Reset();
-    qp.buildOptimisationProblem();
+    qp.buildOptimisationProblem(); //reading the problem and filling in the matrices
     std::cout << "qp.buildOptimisationProblem() time: " << std::fixed << timer.Elapsed().count()/1E6 << "ms\n";
 
     timer.Reset();
-    bool success = (qp.solve() == 0);
+    bool success = (qp.solve() == 0); // solving the problem
     std::cout << "qp.solve() time: " << std::fixed << timer.Elapsed().count()/1E6 << "ms\n";
     if(!success)
     {
@@ -246,14 +263,14 @@ int main(int argc, char const *argv[])
         return -2;
     }
     timer.Reset();
-    
-    
+
+
     int ntrials=1000;
     int i=ntrials;
     do
     {
 
-        
+
         dynConstr.update();
 
         qp.buildOptimisationProblem();
@@ -266,7 +283,7 @@ int main(int argc, char const *argv[])
 
     if(success)
     {
-        std::cout << "Solve succeded : \n" << qp.getPrimalSolution() << '\n';
+        std::cout << "Solve succeded : \n" << qp.getPrimalSolution() << '\n'; //Get the solution
     }
     else
     {
