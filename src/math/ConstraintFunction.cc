ff60e5ae0232bb6e8e7eef782e3dcaf9cb6b075b
Antoine Hoarau <hoarau.robotics@gmail.com>
2018-06-01 01:38:07 +0200

    use orca throws

--- src/math/ConstraintFunction.cc
+++ src/math/ConstraintFunction.cc
@@ -2,14 +2,15 @@
 #include "orca/utils/Utils.h"
 
 using namespace orca::math;
+using namespace orca::utils;
 
 void ConstraintFunction::print() const
 {
     std::cout << "Constraint Matrix C size " << getSize() << std::endl;
     std::cout << getConstraintMatrix() << std::endl;
-    std::cout << "Lower Bound lb" << std::endl;
+    std::cout << "  Lower Bound lb" << std::endl;
     std::cout << getLowerBound() << std::endl;
-    std::cout << "Upper Bound ub" << std::endl;
+    std::cout << "  Upper Bound ub" << std::endl;
     std::cout << getUpperBound() << std::endl;
 }
 
@@ -31,8 +32,8 @@ void ConstraintFunction::setLowerBound(const Eigen::VectorXd& newlb)
     }
     else
     {
-        throw std::runtime_error(orca::utils::Formatter() << "Size of lower bound do not match, provided "
-            << newlb.size() << ", but expected " << lower_bound_.size());
+        LOG_ERROR << "Size of lower bound do not match, provided "
+            << newlb.size() << ", but expected " << lower_bound_.size();
     }
 }
 
@@ -44,8 +45,8 @@ void ConstraintFunction::setUpperBound(const Eigen::VectorXd& newub)
     }
     else
     {
-        throw std::runtime_error(orca::utils::Formatter() << "Size of upper bound do not match, provided "
-            << newub.size() << ", but expected " << upper_bound_.size());
+        LOG_ERROR << "Size of upper bound do not match, provided "
+            << newub.size() << ", but expected " << upper_bound_.size();
     }
 }
 
@@ -57,8 +58,8 @@ void ConstraintFunction::setConstraintMatrix(const Eigen::MatrixXd& newC)
     }
     else
     {
-        throw std::runtime_error(orca::utils::Formatter() << "Size of constraint matrix do not match, provided "
-            << Size(newC) << ", but expected " << getSize());
+        LOG_ERROR << "Size of constraint matrix do not match, provided "
+            << Size(newC) << ", but expected " << getSize();
     }
 }
 
