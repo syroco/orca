19fc605eddf25fdf2888a10c16a3415261bc6882
Antoine Hoarau <hoarau.robotics@gmail.com>
2018-09-13 11:57:38 +0200

    move to cpp file

--- include/orca/common/Config.h
+++ include/orca/common/Config.h
@@ -16,19 +16,12 @@ namespace orca
             using Ptr  = std::shared_ptr<Config>;
             using ParamMap = std::map<std::string, ParameterBase* >;
             
-            Config(const std::string& config_name)
-            : name_(config_name)
-            {
-                LOG_DEBUG << "[" << getName() << "] " << "Starting new config";
-            }
+            Config(const std::string& config_name);
 
             /**
             * @brief Returns the name of the config. Usually the name of the task owning the config.
             */
-            const std::string& getName() const
-            {
-                return name_;
-            }
+            const std::string& getName() const;
             /**
             * @brief Returns true if all params added with @addParameter have been set
             *
@@ -37,25 +30,7 @@ namespace orca
             void addParameter(const std::string& param_name,ParameterBase* param
                     , ParamPolicy policy = ParamPolicy::Required
                     , std::function<void()> on_loading_success = 0
-                    , std::function<void()> on_loading_failed = 0)
-            {
-                if(param_name.empty())
-                    utils::orca_throw(utils::Formatter() << "Cannot have an empty parameter name !");
-                
-                if(!param)
-                    utils::orca_throw(utils::Formatter() << "Param is null !");
-                
-                if(utils::key_exists(parameters_,param_name))
-                {
-                    LOG_ERROR << "[" << getName() << "] " << "Parameter " << param_name << " already declared !";
-                    return;
-                }
-                param->setName(param_name);
-                param->setRequired(policy == ParamPolicy::Required);
-                param->onLoadingSuccess(on_loading_success);
-                param->onLoadingFailed(on_loading_failed);
-                parameters_[param_name] = param;
-            }
+                    , std::function<void()> on_loading_failed = 0);
 
             /**
             * @brief Returns a param via its name.
@@ -63,150 +38,17 @@ namespace orca
             * @param param_name The name of the param (might not exist)
             * @return orca::common::ParameterBase* The param pointer, nullptr if if does not exists
             */
-            ParameterBase* getParameter(const std::string& param_name)
-            {
-                if(!utils::key_exists(parameters_,param_name))
-                {
-                    LOG_ERROR << "[" << getName() << "] " << "Parameter " << param_name << " does not exists !";
-                    return nullptr;
-                }
-                return parameters_[param_name];
-            }
+            ParameterBase* getParameter(const std::string& param_name);
 
-            void print() const
-            {
-                if(parameters_.empty())
-                {
-                    LOG_INFO << "[" << getName() << "] " << "No parameters declared !";
-                    return;
-                }
-                std::stringstream ss;
-                ss << "[" << getName() << "] Parameters :\n";
-                for(auto p : parameters_)
-                {
-                    ss << " * " << p.second->getName()
-                        << "\n      - is required " << std::boolalpha << p.second->isRequired() 
-                        << "\n      - is set " << std::boolalpha << p.second->isSet() << '\n'; 
-                }
-                LOG_INFO << ss.str();
-            }
+            void print() const;
 
-            bool loadFromFile(const std::string& yaml_url)
-            {
-                YAML::Node config = YAML::LoadFile(yaml_url);
-                
-                YAML::Emitter out;
-                out << config;
-                
-                return loadFromString(out.c_str());
-            }
+            bool loadFromFile(const std::string& yaml_url);
 
-            bool loadFromString(const std::string& yaml_str)
-            {
-                LOG_INFO << "[" << getName() << "] Starting configuring from file";
-                if(parameters_.empty())
-                {
-                    LOG_ERROR << "[" << getName() << "] " << "No parameters declared with addParam!";
-                    return false;
-                }
-                // This can throw an exception
-                // Is it good to keep it ?
-                YAML::Node config;
-                try {
-                    config = YAML::Load(yaml_str);
-                } catch(std::exception& e) {
-                    utils::orca_throw(utils::Formatter() << e.what() << "\nYaml file does not seem to be valid.");
-                }
-                
-                if(!config)
-                {
-                    return false;
-                }
-                if(!(config.IsMap() || config.IsSequence()))
-                {
-                    return false;
-                }
-                
-                auto to_string = [](const YAML::Node& n) -> std::string 
-                        { YAML::Emitter out; out << n; return out.c_str(); };
-
-                std::cout << "Configuring from config " << to_string(config) << '\n';
-                
-                for(auto c : config)
-                {
-                    std::cout << "Analysing subconfig " << to_string(c.first) << '\n';
-                    
-                    auto param_name = c.first.as<std::string>();
-                    // Special case for the 'type' param
-                    if(param_name == "type")
-                    {
-                        continue;
-                    }
-                    
-                    if(!utils::key_exists(parameters_,param_name))
-                    {
-                        std::stringstream ss;
-                        ss << "[" << getName() << "] " 
-                            << "Parameter \"" << param_name << "\" not declared but present in the yaml file\n"
-                            << "Did you forget to addParameter() in the component constructor ?\n"
-                            << "Declared parameters : \n";
-                        for(auto p : parameters_)
-                        {
-                            ss << " - " << p.second->getName() << '\n';
-                        }
-                        LOG_WARNING << ss.str();
-                        //return false;
-                    }
-                    else
-                    {
-                        auto param = parameters_[param_name];
-                        if(param->loadFromString(to_string(c.second)))
-                        {
-                            LOG_INFO << "[" << getName() << "] " << "Parameter \"" << param_name << "\" --> " << c.second;
-                        }
-                        else
-                        {
-                            LOG_ERROR << "[" << getName() << "] " << "Could not load \"" << param_name << "\"";
-                        }
-                    }
-                }
-                bool all_set = areAllRequiredParametersSet();
-                if(!all_set)
-                {
-                    print();
-                    std::stringstream ss;
-
-                    for(auto p : parameters_)
-                    {
-                        LOG_WARNING_IF(p.second->isRequired() && ! p.second->isSet()) << "[" << getName() << "] "
-                                << "Required parameter \"" << p.second->getName() 
-                                << "\" is not set";
-                    }
-                    LOG_WARNING << "[" << getName() << "] " << "Configuring failed";
-                }
-                
-                LOG_INFO_IF(all_set) << "[" << getName() << "] " << "Sucessfully configured";
-                
-                return all_set;
-            }
+            bool loadFromString(const std::string& yaml_str);
             
-            bool areAllRequiredParametersSet() const
-            {
-                bool ok = true;
-                for(auto p : parameters_)
-                {
-                    if(p.second->isRequired())
-                    {
-                        ok &= p.second->isSet();
-                    }
-                }
-                return ok;
-            }
+            bool areAllRequiredParametersSet() const;
 
-            const ParamMap& getAllParameters() const
-            {
-                return parameters_;
-            }
+            const ParamMap& getAllParameters() const;
         private:
             std::string name_;
             ParamMap parameters_;
---
+++ src/common/Config.cc
@@ -0,0 +1,197 @@
+#include <orca/common/Config.h>
+
+using namespace orca::common;
+using namespace orca::utils;
+
+Config::Config(const std::string& config_name)
+: name_(config_name)
+{
+    LOG_DEBUG << "[" << getName() << "] " << "Starting new config";
+}
+
+
+const std::string& Config::getName() const
+{
+    return name_;
+}
+
+void Config::addParameter(const std::string& param_name,ParameterBase* param
+        , ParamPolicy policy /*= ParamPolicy::Required*/
+        , std::function<void()> on_loading_success /*= 0*/
+        , std::function<void()> on_loading_failed /*= 0*/)
+{
+    if(param_name.empty())
+        orca_throw(Formatter() << "Cannot have an empty parameter name !");
+    
+    if(!param)
+        orca_throw(Formatter() << "Param is null !");
+    
+    if(key_exists(parameters_,param_name))
+    {
+        LOG_ERROR << "[" << getName() << "] " << "Parameter " << param_name << " already declared !";
+        return;
+    }
+    param->setName(param_name);
+    param->setRequired(policy == ParamPolicy::Required);
+    param->onLoadingSuccess(on_loading_success);
+    param->onLoadingFailed(on_loading_failed);
+    parameters_[param_name] = param;
+}
+
+
+ParameterBase* Config::getParameter(const std::string& param_name)
+{
+    if(!key_exists(parameters_,param_name))
+    {
+        LOG_ERROR << "[" << getName() << "] " << "Parameter " << param_name << " does not exists !";
+        return nullptr;
+    }
+    return parameters_[param_name];
+}
+
+void Config::print() const
+{
+    if(parameters_.empty())
+    {
+        LOG_INFO << "[" << getName() << "] " << "No parameters declared !";
+        return;
+    }
+    std::stringstream ss;
+    ss << "[" << getName() << "] Parameters :\n";
+    for(auto p : parameters_)
+    {
+        ss << " * " << p.second->getName()
+            << "\n      - is required " << std::boolalpha << p.second->isRequired() 
+            << "\n      - is set " << std::boolalpha << p.second->isSet() << '\n'; 
+    }
+    LOG_INFO << ss.str();
+}
+
+bool Config::loadFromFile(const std::string& yaml_url)
+{
+    YAML::Node config;
+    try {
+        config = YAML::LoadFile(yaml_url);
+    } catch(std::exception& e) {
+        orca_throw(Formatter() << e.what() << "\nYaml url [" << yaml_url <<"] does not seem to be valid.");
+    }
+
+    YAML::Emitter out;
+    out << config;
+    
+    return loadFromString(out.c_str());
+}
+
+bool Config::loadFromString(const std::string& yaml_str)
+{
+    LOG_INFO << "[" << getName() << "] Starting configuring from file";
+    if(parameters_.empty())
+    {
+        LOG_ERROR << "[" << getName() << "] " << "No parameters declared with addParam!";
+        return false;
+    }
+    // This can throw an exception
+    // Is it good to keep it ?
+    YAML::Node config;
+    try {
+        config = YAML::Load(yaml_str);
+    } catch(std::exception& e) {
+        orca_throw(Formatter() << e.what() << "\nYaml file does not seem to be valid, usually bad formatting.\n\n" << yaml_str);
+    }
+    // For now we pass the node value to the config. Maybe add the key also to check if it corresponds to
+    // the config name ?
+//                 auto config_name = config.first.as<std::string>();
+//                 if(config_name != this->getName())
+//                 {
+//                     orca_throw(Formatter() << "YAML Node name (" << config_name << ") does not match with config name (" << name_ ")");
+//                 }
+    
+    if(!config)
+    {
+        return false;
+    }
+    if(!(config.IsMap() || config.IsSequence()))
+    {
+        return false;
+    }
+    
+    auto to_string = [](const YAML::Node& n) -> std::string 
+            { YAML::Emitter out; out << n; return out.c_str(); };
+
+    std::cout << "Configuring from config " << to_string(config) << '\n';
+    
+    for(auto c : config)
+    {
+        std::cout << "Analysing subconfig " << to_string(c.first) << '\n';
+        
+        auto param_name = c.first.as<std::string>();
+        // Special case for the 'type' param
+        if(param_name == "type")
+        {
+            continue;
+        }
+        
+        if(!key_exists(parameters_,param_name))
+        {
+            std::stringstream ss;
+            ss << "[" << getName() << "] " 
+                << "Parameter \"" << param_name << "\" not declared but present in the yaml file\n"
+                << "Did you forget to addParameter() in the component constructor ?\n"
+                << "Declared parameters : \n";
+            for(auto p : parameters_)
+            {
+                ss << " - " << p.second->getName() << '\n';
+            }
+            LOG_WARNING << ss.str();
+            //return false;
+        }
+        else
+        {
+            auto param = parameters_[param_name];
+            if(param->loadFromString(to_string(c.second)))
+            {
+                LOG_INFO << "[" << getName() << "] " << "Parameter \"" << param_name << "\" --> " << c.second;
+            }
+            else
+            {
+                LOG_ERROR << "[" << getName() << "] " << "Could not load \"" << param_name << "\"";
+            }
+        }
+    }
+    bool all_set = areAllRequiredParametersSet();
+    if(!all_set)
+    {
+        print();
+        std::stringstream ss;
+
+        for(auto p : parameters_)
+        {
+            LOG_WARNING_IF(p.second->isRequired() && ! p.second->isSet()) << "[" << getName() << "] "
+                    << "Required parameter \"" << p.second->getName() 
+                    << "\" is not set";
+        }
+        LOG_WARNING << "[" << getName() << "] " << "Configuring failed";
+    }
+    
+    LOG_INFO_IF(all_set) << "[" << getName() << "] " << "Sucessfully configured";
+    
+    return all_set;
+}
+
+bool Config::areAllRequiredParametersSet() const
+{
+    bool ok = true;
+    for(auto p : parameters_)
+    {
+        if(p.second->isRequired())
+        {
+            ok &= p.second->isSet();
+        }
+    }
+    return ok;
+}
+
+const Config::ParamMap& Config::getAllParameters() const
+{
+    return parameters_;
+}
